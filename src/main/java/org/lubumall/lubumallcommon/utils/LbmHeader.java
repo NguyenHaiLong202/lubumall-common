1eeeb85aafb457a1b84efc04cacf741bca654e89
longnh <longjava2024@gmail.com>
2025-03-24 09:06:39 +0700

    fix

---
+++ src/main/java/org/lubumall/lubumallcommon/utils/LbmHeader.java
@@ -0,0 +1,12 @@
+package org.lubumall.lubumallcommon.utils;
+
+public class LbmHeader {
+    public static final String headerPrefix = "x-lbm";
+    public static final String shopId = headerPrefix + "-shop-id";
+    public static final String shopName = headerPrefix + "-shop-name";
+    public static final String shopStatus = headerPrefix + "-shop-status";
+    public static final String userId = headerPrefix + "-user-id";
+    public static final String username = headerPrefix + "-username";
+    public static final String email = headerPrefix + "-email";
+    public static final String userRole = headerPrefix + "-user-role";
+}
